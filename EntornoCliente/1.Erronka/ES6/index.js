d4a65d495118e19bce59cd59d472d39c2c488983
Aimarcapo <alonso.aimar@uni.eus>
2024-10-01 13:24:40 +0200

    Javascript ES6

---
+++ EntornoCliente/1.Erronka/ES6/index.js
@@ -0,0 +1,36 @@
+// Constructor de Estudiante en ES5 function
+class Estudiante {
+    constructor(nombre, edad, ...asignaturas) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.asignaturas = asignaturas;
+    }
+
+
+    saludar = () => {
+        console.log(`Hola, me llamo ${this.nombre} y tengo ${this.edad} años.`);
+    }
+
+
+    listarAsignaturas = () => {
+        console.log(`Mis asignaturas son: ${this.asignaturas.join(', ')}`);
+    }
+}
+
+const estudiante1 = new Estudiante('Ana', 20, 'Matemáticas', 'Historia', 'Literatura');
+const {nombre, edad} = estudiante1;
+// Destructuring manual para extraer nombre y edad
+
+console.log('Nombre: ' + nombre);
+console.log('Edad: ' + edad);
+estudiante1.saludar();
+estudiante1.listarAsignaturas();
+
+const calcularPromedio = (...notas) => {
+    const suma = notas.reduce((acc, nota) => acc + nota, 0);
+    return suma / notas.length;
+}
+
+// Calcular promedio de notas
+const promedio = calcularPromedio(85, 90, 78, 92);
+console.log(`El promedio es: ${promedio}`);
