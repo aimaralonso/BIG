5eb178c16d7e3f0302a977e9d5c70d858f3e853c
Aimarcapo <alonso.aimar@uni.eus>
2024-11-21 08:17:20 +0100

    Angular unit

--- EntornoCliente/2.Erronka/nireApp/src/app/app.module.ts
+++ EntornoCliente/2.Erronka/nireApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NireOsagaiaComponent } from './nire-osagaia/nire-osagaia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NireOsagaiaComponent
   ],
   imports: [
     BrowserModule,
---
+++ EntornoCliente/2.Erronka/nireApp/src/app/nire-osagaia/nire-osagaia.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nire-osagaia',
+  templateUrl: './nire-osagaia.component.html',
+  styleUrl: './nire-osagaia.component.css',
+})
+export class NireOsagaiaComponent {
+  langilea = {
+    izena: 'Pepe',
+    abizena: 'Perez',
+    generoa: 'gizona',
+    irudia: '/assets/images/caraMujer.jpg',
+  };
+  Title = 'Langileen zerrenda';
+  getTitle() {
+    return this.Title;
+  }
+  langileak = [
+    { izena: 'Pepe', abizena: 'Perez', generoa: 'gizona', soldata: 1000 },
+    { izena: 'Sara', abizena: 'Ruiz', generoa: 'emakumea', soldata: 1000 },
+    { izena: 'Paul', abizena: 'Elorza', generoa: 'gizona', soldata: 1500 },
+    { izena: 'Raul', abizena: 'Txakartegi', generoa: 'gizona', soldata: 2000 },
+  ];
+}
