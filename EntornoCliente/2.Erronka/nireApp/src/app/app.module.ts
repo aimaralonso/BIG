d96360c38b31f904f290537bc0b53bba1d42942f
Aimarcapo <alonso.aimar@uni.eus>
2024-11-26 08:44:20 +0100

    Angular

--- EntornoCliente/2.Erronka/nireApp/src/app/app.module.ts
+++ EntornoCliente/2.Erronka/nireApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NireOsagaiaComponent } from './nire-osagaia/nire-osagaia.component';
@@ -12,7 +12,8 @@ import { NireOsagaiaComponent } from './nire-osagaia/nire-osagaia.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule      
   ],
   providers: [
     provideClientHydration()
--- EntornoCliente/2.Erronka/nireApp/src/app/nire-osagaia/nire-osagaia.component.ts
+++ EntornoCliente/2.Erronka/nireApp/src/app/nire-osagaia/nire-osagaia.component.ts
@@ -6,20 +6,64 @@ import { Component } from '@angular/core';
   styleUrl: './nire-osagaia.component.css',
 })
 export class NireOsagaiaComponent {
+  rowStart = 0;
+  rowLimit = 3;
   langilea = {
     izena: 'Pepe',
     abizena: 'Perez',
     generoa: 'gizona',
     irudia: '/assets/images/caraMujer.jpg',
+    likes: 0,
   };
   Title = 'Langileen zerrenda';
   getTitle() {
     return this.Title;
   }
   langileak = [
-    { izena: 'Pepe', abizena: 'Perez', generoa: 'gizona', soldata: 1000 },
-    { izena: 'Sara', abizena: 'Ruiz', generoa: 'emakumea', soldata: 1000 },
-    { izena: 'Paul', abizena: 'Elorza', generoa: 'gizona', soldata: 1500 },
-    { izena: 'Raul', abizena: 'Txakartegi', generoa: 'gizona', soldata: 2000 },
+    {
+      izena: 'Pepe',
+      abizena: 'Perez',
+      generoa: 'gizona',
+      soldata: 1000,
+      likes: 0,
+      lanean: true,
+    },
+    {
+      izena: 'Sara',
+      abizena: 'Ruiz',
+      generoa: 'emakumea',
+      soldata: 1000,
+      likes: 0,
+      lanean: true,
+    },
+    {
+      izena: 'Paul',
+      abizena: 'Elorza',
+      generoa: 'gizona',
+      soldata: 1500,
+      likes: 0,
+      lanean: true,
+    },
+    {
+      izena: 'Raul',
+      abizena: 'Txakartegi',
+      generoa: 'gizona',
+      soldata: 2000,
+      likes: 0,
+      lanean: true,
+    },
   ];
+  gehituLikes(langilea: any) {
+    langilea.likes++;
+    console.log(`Likes aumentados: ${langilea.likes} para ${langilea.izena}`);
+  }
+  kenduRow() {
+    this.rowStart -= 2;
+    this.rowLimit -= 2;
+  }
+  gehituRow() {
+    this.rowStart += 2;
+    this.rowLimit += 2;
+  }
+  myText = 'Nire izena: Aimar da';
 }
